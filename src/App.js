5318ffd5c27b2cced8f6ef006df323157b64cb30
RAFAEL VALDEZ <manrvaldez.92@gmail.com>
2022-02-26 16:02:57 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    var video = document.getElementById('myvideo');
+    video.play();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='video-container'>
+        <video
+          id='myvideo'
+          src='video_home.mp4'
+          className='video'
+          loop
+          autoplay
+          muted
+        />
+      </div>
     </div>
   );
 }
